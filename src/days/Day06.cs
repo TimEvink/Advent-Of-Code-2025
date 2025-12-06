b91d7133bf1170bb89f795e8a3b694fb37731190
Tim Evink <timevink@gmail.com>
2025-12-06 13:31:57 +0100

    day 6

---
+++ src/days/Day06.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using aoc2025.src.interfaces;
+
+namespace aoc2025.src.Days;
+public class Day06 : IDay {
+    private readonly string _inputPath;
+    public Day06(string inputPath = "") {
+        _inputPath = inputPath;
+    }
+
+    private static readonly Dictionary<char, Func<long, long, long>> operations = new Dictionary<char, Func<long, long, long>> {
+        ['+'] = (a, b) => a + b,
+        ['*'] = (a, b) => a * b
+    };
+
+    public string[][] ParseInput() {
+        return File.ReadLines(_inputPath).Select(line => Regex.Split(line.Trim(), @"\s+")).ToArray();
+    }
+
+    public string[] ParseInputRaw() {
+        return File.ReadAllLines(_inputPath);
+    }
+
+    public object SolvePart1() {
+        var input = ParseInput();
+        int rownumber = input.Length;
+        char[] operationchars = input[rownumber - 1].Select(s => s[0]).ToArray();
+        long counter = 0;
+        for (int i = 0; i < input[0].Length; i++) {
+            var iterator = Enumerable.Range(0, input.Length - 1).Select(j => long.Parse(input[j][i]));
+            counter += iterator.Aggregate(operations[operationchars[i]]);
+        }
+        return counter;
+    }
+
+    public long? CephalopodNumber(string[] input, int index) {
+        IEnumerable<char> chars = Enumerable.Range(0, input.Length - 1)
+            .Select(i => input[i][index])
+            .Where(c => c !=' ');
+        return long.TryParse(new string(chars.ToArray()), out var result) ? result: null;
+    }
+
+    public object SolvePart2() {
+        var input = ParseInputRaw();
+        int rownumber = input.Length;
+        long totalcount = 0;
+        char operation = input[rownumber - 1][0];
+        List<long> numbers = new();
+        for (int index = 0; index < input[0].Length; index++) {
+            long? number = CephalopodNumber(input, index);
+            if (number == null) {
+                totalcount += numbers.Aggregate(operations[operation]);
+                numbers.Clear();
+                continue;
+            }
+            char potentialnewoperation = input[rownumber - 1][index];
+            if (potentialnewoperation != ' ') operation = potentialnewoperation;
+            numbers.Add(number.Value);
+        }
+        totalcount += numbers.Aggregate(operations[operation]);
+        return totalcount;
+    }
+}
---
+++ tests/days/Day06Tests.cs
@@ -0,0 +1,38 @@
+﻿using Xunit;
+using aoc2025.src.Days;
+
+namespace aoc2025.tests.Days;
+public class Day06Tests {
+    [Fact]
+    public void ParseInputTest() {
+        var day = new Day06("testinput/day06/parseinputtest.txt");
+        Assert.Equal(new[] {
+            new string[] {"123", "328", "51"},
+            new string[] {"45", "64", "387"},
+            new string[] {"*", "+", "*"}
+        }, day.ParseInput());
+    }
+    [Fact]
+    public void Example1Test() {
+        var day = new Day06("testinput/day06/example.txt");
+        Assert.Equal(4277556L, day.SolvePart1());
+    }
+
+    [Theory]
+    [InlineData(0, 1)]
+    [InlineData(1, 24)]
+    [InlineData(2, 356)]
+    [InlineData(4, 369)]
+    [InlineData(6, 8)]
+    public void CephalopodNumberTest(int index, long expected) {
+        var day = new Day06("testinput/day06/example.txt");
+        var input = day.ParseInputRaw();
+        Assert.Equal(expected, day.CephalopodNumber(input, index));
+    }
+
+    [Fact]
+    public void Example2Test() {
+        var day = new Day06("testinput/day06/example.txt");
+        Assert.Equal(3263827L, day.SolvePart2());
+    }
+}
